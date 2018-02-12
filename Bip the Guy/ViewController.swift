e869a013be8ef9e10b007ba780c5fb41ad458767
Amanda Zhao <amanda.zhao1@amandas-macbook-pro-2.local>
2018-02-12 16:23:51 -0500

    Class 4 Commit

--- Bip the Guy/ViewController.swift
+++ Bip the Guy/ViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var ImageToPunch: UIImageView!
+    
+    var audioPlayer = AVAudioPlayer()
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("view.frame = \(view.frame)")
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +29,83 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Functions
+    func animateImage() {
+        let bounds = self.ImageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        
+        //shrink our ImageToPunch by 60p
+        self.ImageToPunch.bounds = CGRect(x: self.ImageToPunch.bounds.origin.x + shrinkValue, y: self.ImageToPunch.bounds.origin.y + shrinkValue, width: self.ImageToPunch.bounds.size.width - shrinkValue, height: self.ImageToPunch.bounds.size.height - shrinkValue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.ImageToPunch.bounds = bounds }, completion: nil)
+    }
+    
+    
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        //Can we load in the file soundName?
+        if let sound = NSDataAsset(name: soundName) {
+            //check if sound.data is a sound file
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                //if sound.data is not a valid audio file
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        } else {
+            //if reading in the NSDataAsset didn't work, tell the user / report the error.
+            print("ERROR: file \(soundName) didn't load")
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        ImageToPunch.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: Actions
+    @IBAction func libraryPressed(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            
+            imagePicker.delegate = self
+            
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            showAlert(title: "Camera Not Available", message: "There is no camera available on this device.")
 
+    }
+    
+    }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
+    }
+    
+    
 }
 
